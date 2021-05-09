2152af73ae6cd55ca0f550298e0358496acb126f
Vitaliy Bernyk <65338532+IartemidaI@users.noreply.github.com>
2021-05-09 17:41:06 +0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+apt update
+apt install docker.io -y
+curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
